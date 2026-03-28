12a5b0daa9cbe1c91ceeed9a8ebe43f4a6c775d9
henrikbrekken <henrik.brekken@gmail.com>
2026-03-28 16:30:27 +0100

    updated to use 3 dof vectors form vessel

--- src/vessel/polar_transformation.py
+++ src/vessel/polar_transformation.py
@@ -8,7 +8,9 @@ class polarTransformation:
 
     def __init__(self, vessel: Vessel, x0=0, y0=0):
         self.vessel: Vessel = vessel
-        self.update_states()
+        self.x0 = x0
+        self.y0 = y0
+        self._update_states()
 
     def _update_x0(self, x0):
         self.x0 = x0
@@ -17,14 +19,15 @@ class polarTransformation:
         self.y0 = y0
 
     def _update_generalized_coordinates(self):
-        dx = self.vessel.eta[0] - self.x0
-        dy = self.vessel.eta[1] - self.y0
+        dx = self.vessel.eta_3DOF[0] - self.x0
+        dy = self.vessel.eta_3DOF[1] - self.y0
 
         self.rho = np.sqrt((dx) ** 2 + (dy) ** 2)
         self.gamma = np.atan2(dy, dx)
-        self.psi = self.vessel.eta[5]
+        self.psi = self.vessel.eta_3DOF[2]
 
         self.eta = np.array([self.rho, self.gamma, self.psi])
+        self.nu = self.vessel.nu_3DOF
 
     def _update_T(self):
         a = self.psi - self.gamma
@@ -54,9 +57,15 @@ class polarTransformation:
     def _update_D(self):
         self.D = self.Tinv @ self.vessel.D_3DOF @ self.T
 
-    def update_states(self, x0=0, y0=0):
+    def update_polar_frame_co(self, x0, y0):
         self._update_x0(x0)
         self._update_y0(y0)
+
+    def integrate_dynamics(self, dt, tau, Vc, betaVc):
+        self.vessel.integrate_dynamics(dt, tau, Vc, betaVc)
+        self._update_states()
+
+    def _update_states(self):
         self._update_generalized_coordinates()
         self._update_T()
         self._update_M()
