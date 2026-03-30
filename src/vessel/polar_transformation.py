aed54de323558e22c96347314c8710cd7cef64c5
henrikbrekken <henrik.brekken@gmail.com>
2026-03-30 18:39:02 +0200

    typing

--- src/vessel/polar_transformation.py
+++ src/vessel/polar_transformation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.typing as npt
 
 from vessel.vessel import Vessel
 
@@ -22,23 +23,23 @@ class polarTransformation:
         dx = self.vessel.eta_3DOF[0] - self.x0
         dy = self.vessel.eta_3DOF[1] - self.y0
 
-        self.rho = np.sqrt((dx) ** 2 + (dy) ** 2)
-        self.gamma = np.atan2(dy, dx)
-        self.psi = self.vessel.eta_3DOF[2]
+        self.rho: np.float64 = np.sqrt(dx ** 2 + dy ** 2)
+        self.gamma: np.float64 = np.atan2(dy, dx)
+        self.psi: np.float64 = self.vessel.eta_3DOF[2]
 
-        self.eta = np.array([self.rho, self.gamma, self.psi])
-        self.nu = self.vessel.nu_3DOF
+        self.eta: npt.NDArray[np.float64] = np.array([self.rho, self.gamma, self.psi])
+        self.nu: npt.NDArray[np.float64] = self.vessel.nu_3DOF
 
     def _update_T(self):
         a = self.psi - self.gamma
-        self.T = np.array(
+        self.T: npt.NDArray[np.float64] = np.array(
             [
                 [np.cos(a), -np.sin(a), 0],
                 [1 / self.rho * np.sin(a), 1 / self.rho * np.cos(a), 0],
                 [0, 0, 1],
             ]
         )
-        self.Tinv = np.array(
+        self.Tinv: npt.NDArray[np.float64] = np.array(
             [
                 [np.cos(a), self.rho * np.sin(a), 0],
                 [-np.sin(a), self.rho * np.cos(a), 0],
@@ -47,15 +48,13 @@ class polarTransformation:
         )
 
     def _update_eta_dot(self):
-        self.eta_dot = self.T @ np.array(
-            [self.vessel.nu[0], self.vessel.nu[1], self.vessel.nu[5]]
-        )
+        self.eta_dot: npt.NDArray[np.float64] = self.T @ self.vessel.nu_3DOF
 
     def _update_M(self):
-        self.M = self.Tinv @ self.vessel.M_3DOF @ self.T
+        self.M: npt.NDArray[np.float64] = self.Tinv.T @ self.vessel.M_3DOF @ self.Tinv
 
     def _update_D(self):
-        self.D = self.Tinv @ self.vessel.D_3DOF @ self.T
+        self.D: npt.NDArray[np.float64] = self.Tinv.T @ self.vessel.D_3DOF @ self.Tinv
 
     def update_polar_frame_co(self, x0, y0):
         self._update_x0(x0)
@@ -68,5 +67,6 @@ class polarTransformation:
     def _update_states(self):
         self._update_generalized_coordinates()
         self._update_T()
+        self._update_eta_dot()
         self._update_M()
         self._update_D()
